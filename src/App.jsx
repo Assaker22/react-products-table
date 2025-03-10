f3c9d11f0bca73b97c17e09d489440091ef0e7c4
Charbel Assaker <charbelassaker@live.com>
2025-03-10 17:27:00 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// The app should include a table of products, fetched from
+// an api: https://dummyjson.com/products.
+
+// Then, add a search FileSystemEntry, to search for
+// products by name, using:
+// https://dummyjson.com/products/search?q=phone
+
+// No libraries are allowed, except for React and its
+// dependencies.
+
+export default function App() {
+  return "Welcome :)";
+}
